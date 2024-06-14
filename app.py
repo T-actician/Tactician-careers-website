de0f9248b24c62cbfad894b240ff9166c39c4e1a
Tactician <167181702+T-actician@users.noreply.github.com>
2024-06-14 09:32:47 +0000

    Updated app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from re import DEBUG
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+  return "The TECHtician"
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=8080, debug=True)
